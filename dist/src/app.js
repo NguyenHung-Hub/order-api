9b2470f8bc506d4c354f173a0743061f9476f9d2
Hung <daminhnguyenhung@gmail.com>
2023-07-20 16:23:09 +0700

    anable middleware set header

--- dist/src/app.js
+++ dist/src/app.js
@@ -29,15 +29,12 @@ app.use("/v1", v1_1.default);
 app.use(error_1.errorHandlerCall);
 app.use(error_1.errorHandler);
 (0, database_1.default)();
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     res.header(
-//         "Access-Control-Allow-Methods",
-//         "PUT, GET, POST, DELETE, OPTIONS"
-//     );
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header("Access-Control-Allow-Methods", "PUT, GET, POST, DELETE, OPTIONS");
+    next();
+});
 exports.default = app;
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -31,14 +31,14 @@ app.use(errorHandlerCall);
 app.use(errorHandler);
 
 connectDB();
-// app.use(function (req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "X-Requested-With");
-//     res.header("Access-Control-Allow-Headers", "Content-Type");
-//     res.header(
-//         "Access-Control-Allow-Methods",
-//         "PUT, GET, POST, DELETE, OPTIONS"
-//     );
-//     next();
-// });
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    res.header("Access-Control-Allow-Headers", "Content-Type");
+    res.header(
+        "Access-Control-Allow-Methods",
+        "PUT, GET, POST, DELETE, OPTIONS"
+    );
+    next();
+});
 export default app;
